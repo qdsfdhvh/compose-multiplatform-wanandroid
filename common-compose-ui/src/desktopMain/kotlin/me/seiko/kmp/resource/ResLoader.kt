66c5ace1c6bde33d60bbc22f02be09ee81615e65
seiko <605590140@qq.com>
2021-11-28 18:43:29 +0800

    optIn ExperimentalComposeUiApi

--- common-compose-ui/src/desktopMain/kotlin/me/seiko/kmp/resource/ResLoader.kt
+++ common-compose-ui/src/desktopMain/kotlin/me/seiko/kmp/resource/ResLoader.kt
@@ -1,12 +1,14 @@
 package me.seiko.kmp.resource
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import dev.icerock.moko.resources.FileResource
 import dev.icerock.moko.resources.ImageResource
 import dev.icerock.moko.resources.StringResource
 
+@OptIn(ExperimentalComposeUiApi::class)
 actual class ResLoader {
 
   actual fun getString(res: StringResource, vararg args: Any): String {
