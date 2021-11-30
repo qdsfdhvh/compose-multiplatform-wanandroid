4b0d6a1d7a3e34315453018a15cf208dd22dc21c
seiko <605590140@qq.com>
2021-11-30 22:58:02 +0800

    add theme color

--- common-compose-ui/src/commonMain/kotlin/me/seiko/chat/theme/Theme.kt
+++ common-compose-ui/src/commonMain/kotlin/me/seiko/chat/theme/Theme.kt
@@ -1,13 +1,29 @@
 package me.seiko.chat.theme
 
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun AppTheme(
+  darkTheme: Boolean = false,
   content: @Composable () -> Unit
 ) {
-  MaterialTheme {
+  MaterialTheme(if (darkTheme) darkColors else lightColors) {
     content()
   }
 }
+
+val lightColors = lightColors(
+  primary = Color(0XFF1C68F3),
+  primaryVariant = Color(0XFF1C68F3),
+  secondary = Color(0XFF1C68F3),
+)
+
+val darkColors = darkColors(
+  primary = Color(0XFF5CB0FF),
+  primaryVariant = Color(0XFF5CB0FF),
+  secondary = Color(0XFF5CB0FF),
+)
