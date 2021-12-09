1d119324231c02e872e50581ff2fbd0e32ba12f7
seiko <605590140@qq.com>
2021-12-09 11:39:29 +0800

    apply spotless

--- common-compose-ui/src/commonMain/kotlin/me/seiko/chat/di/module/ViewModelModules.kt
+++ common-compose-ui/src/commonMain/kotlin/me/seiko/chat/di/module/ViewModelModules.kt
@@ -4,8 +4,8 @@ import me.seiko.chat.di.extension.viewModel
 import me.seiko.chat.dialog.CustomDialogViewModel
 import me.seiko.chat.scene.detail.DetailViewModel
 import me.seiko.chat.scene.home.HomeViewModel
-import me.seiko.chat.scene.home.bottom.MeViewModel
 import me.seiko.chat.scene.home.bottom.KnowledgeViewModel
+import me.seiko.chat.scene.home.bottom.MeViewModel
 import me.seiko.chat.scene.home.bottom.SearchViewModel
 import me.seiko.chat.scene.home.bottom.TimeLineViewModel
 import org.koin.dsl.module
