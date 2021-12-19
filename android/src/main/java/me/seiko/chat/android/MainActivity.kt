3b8e17527428db5fdf0557465cc9c21a8856658c
seiko <605590140@qq.com>
2021-12-19 19:43:04 +0800

    provide ResLoader

--- android/src/main/java/me/seiko/chat/android/MainActivity.kt
+++ android/src/main/java/me/seiko/chat/android/MainActivity.kt
@@ -2,11 +2,14 @@ package me.seiko.chat.android
 
 import android.graphics.Color
 import android.os.Bundle
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.core.view.WindowCompat
 import me.seiko.chat.App
 import me.seiko.compose.insets.ProvideWindowInsets
 import me.seiko.jetpack.lifecycle.PreComposeActivity
 import me.seiko.jetpack.lifecycle.setContent
+import me.seiko.kmp.LocalResLoader
+import me.seiko.kmp.resource.ResLoader
 
 class MainActivity : PreComposeActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +21,11 @@ class MainActivity : PreComposeActivity() {
 
     setContent {
       ProvideWindowInsets {
-        App()
+        CompositionLocalProvider(
+          LocalResLoader provides ResLoader(this)
+        ) {
+          App()
+        }
       }
     }
   }
--- common-compose-ui/src/desktopMain/kotlin/me/seiko/chat/DesktopApp.kt
+++ common-compose-ui/src/desktopMain/kotlin/me/seiko/chat/DesktopApp.kt
@@ -1,10 +1,17 @@
 package me.seiko.chat
 
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.window.application
 import me.seiko.jetpack.lifecycle.PreComposeWindow
+import me.seiko.kmp.LocalResLoader
+import me.seiko.kmp.resource.ResLoader
 
 fun runDesktop() = application {
   PreComposeWindow(onCloseRequest = ::exitApplication) {
-    App()
+    CompositionLocalProvider(
+      LocalResLoader provides ResLoader()
+    ) {
+      App()
+    }
   }
 }
